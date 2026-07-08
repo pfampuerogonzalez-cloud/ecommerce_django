2d6b5dbe0b84dcd2ccbb18bddff7f7d36af972ea
Pablo <pfampuerogonzalez@gmail.com>
2026-07-08 17:08:01 +0000

    fix: add RENDER_EXTERNAL_HOSTNAME to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-c^v2pox)==jngk
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
     "DJANGO_CSRF_TRUSTED_ORIGINS",
